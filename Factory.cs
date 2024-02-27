a615e254a90197ccde3cec5358b411d0478ffa4c
Saikarr <160714213+Saikarr@users.noreply.github.com>
2024-02-27 21:42:18 +0100

    lab1.1, not finished

---
+++ Factory.cs
@@ -0,0 +1,81 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace Lab1;
+public abstract class Factory
+{
+    public abstract IFactory Make(string[] args);
+    public IFactory GetObject(string[] args) 
+    {
+        return Make(args);
+    }
+}
+
+public class CrewFactory : Factory
+{
+    public override IFactory Make(string[] args)
+    { 
+        return new Crew(args);
+    }
+
+}
+public class PassengerFactory : Factory
+{
+    public override IFactory Make(string[] args)
+    {
+        return new Passenger(args);
+    }
+}
+public class CargoFactory : Factory
+{
+    public override IFactory Make(string[] args)
+    {
+        return new Cargo(args);
+    }
+}
+public class CargoPlaneFactory : Factory
+{
+    public override IFactory Make(string[] args)
+    {
+        return new CargoPlane(args);
+    }
+}
+public class PassengerPlaneFactory : Factory
+{
+    public override IFactory Make(string[] args)
+    {
+        return new PassengerPlane(args);
+    }
+}
+public class AirportFactory : Factory
+{
+    public override IFactory Make(string[] args)
+    {
+        return new Airport(args);
+    }
+}
+public class FlightFactory : Factory
+{
+    public override IFactory Make(string[] args)
+    {
+        return new Flight(args);
+    }
+}
+
+class FactoryDictionary
+{
+    public static Dictionary<string, Factory> factories = new Dictionary<string, Factory>{
+        {"C", new CrewFactory() },
+        {"P", new PassengerFactory() },
+        {"CA", new CargoFactory() },
+        {"CP", new CargoPlaneFactory() },
+        {"PP", new PassengerPlaneFactory() },
+        {"AI", new AirportFactory() },
+        {"FL", new FlightFactory() }
+    };
+
+    public static IFactory Create(string type, string[] data)
+    {
+        return factories[type].Make(data);
+    }
+}
